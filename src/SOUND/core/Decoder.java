878135ae4b993e9fbb5a27f9c37e1f0b16a50c93
Tony@iMac <maoymt@gmail.com>
2018-03-22 10:05:30 +0800

    add some utils. generate the wav

--- src/SOUND/core/Decoder.java
+++ src/SOUND/core/Decoder.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import static java.lang.System.exit;
@@ -13,16 +14,26 @@ public class Decoder {
 
     private static float SAMPLE_RATE = 44100;
     private static int FRAME_SIZE = 92;
-    private static float CARRIER_FREQ = 1000;
+    private static float CARRIER1_FREQ = 10000;
+    private static float CARRIER0_FREQ = 5000;
     private static int PREAMBLE_SIZE = 440;
     private static double CUTOFF_1 = 2e3;
     private static double CUTOFF_2 = 10e3;
     private static double AMP_PREAMPLE = 127;
-
+    private static int BIT_SAMPLE = 44;
+    private static byte[] FRAME0 = new byte[BIT_SAMPLE];
+    private static byte[] FRAME1 = new byte[BIT_SAMPLE];
 
     public Decoder() {
     }
 
+    private static byte[] generateWave(int sample, float carrier) {
+        byte[] wave = new byte[sample];
+        for (int i = 0; i < sample; i++) {
+            wave[i] = (byte) (127 * Math.sin(2 * Math.PI * carrier * i / SAMPLE_RATE));
+        }
+        return wave;
+    }
 
     private static LinkedList getFile(String filePath) {
         LinkedList linkedList = new LinkedList();
@@ -78,16 +89,30 @@ public class Decoder {
     }
 
     public static void main(String args[]) {
-        LinkedList dataList = getFile("/Users/tony_mao/Develop/CS120-Toy/text/input.txt");
+        System.out.println("=> Setup carriers!!!!.....");
+        FRAME0 = generateWave(BIT_SAMPLE, CARRIER0_FREQ);
+        FRAME1 = generateWave(BIT_SAMPLE, CARRIER1_FREQ);
+        LinkedList dataList = getFile("/Users/tony/IdeaProjects/CS120-Toy/text/input.txt");
         int len = dataList.size();
         // TODO: Handle 10Mbit file
         System.out.println(len);
+        utils UTIL = new utils();
+        byte soundTrack[] = getPreamble();
         while (dataList.size() != 0) {
-            System.out.print(dataList.pop());
+            int sig = (int) dataList.pop();
+            if (sig == 0) {
+                soundTrack = UTIL.addArray(soundTrack, FRAME0);
+            } else {
+                soundTrack = UTIL.addArray(soundTrack, FRAME1);
+            }
         }
         System.out.println("=> end debug");
-        System.out.println((byte) 213.111);
-        getPreamble();
+//        System.out.println((byte) 213.111);
+//        System.out.println(Arrays.toString(getPreamble()));
+
+//        System.out.println(Arrays.toString(FRAME0));
+//        System.out.println(Arrays.toString(FRAME1));
+        System.out.println(Arrays.toString(soundTrack));
     }
 
 
--- src/SOUND/core/utils.java
+++ src/SOUND/core/utils.java
@@ -12,6 +12,15 @@ public class utils {
         return ret;
     }
 
+    public static byte[] addArray(byte[] pre, byte[] suc) {
+        if (pre.length == 0) return suc;
+        if (suc.length == 0) return pre;
+        byte ret[] = new byte[pre.length + suc.length];
+        System.arraycopy(pre, 0, ret, 0, pre.length);
+        System.arraycopy(suc, 0, ret, pre.length, suc.length);
+        return ret;
+    }
+
     public static void main(String args[]) {
         System.out.print(Arrays.toString(dec2ByteArr(255, 8)));
     }
